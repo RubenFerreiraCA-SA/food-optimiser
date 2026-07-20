bf17c43ff78f7b66319b82623b61c12f2c7776e6
Ruben <rubenf85@gmail.com>
2026-07-20 22:25:40 +0200

    Draft home page added

---
+++ src/app/hero-illustration/hero-illustration.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-illustration',
+  templateUrl: './hero-illustration.html',
+  styleUrl: './hero-illustration.scss',
+})
+export class HeroIllustration {}
--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeroIllustration } from '../hero-illustration/hero-illustration';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [HeroIllustration],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
