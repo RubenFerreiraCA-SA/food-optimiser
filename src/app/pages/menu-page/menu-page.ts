d77d4b8e7c338fe04cf5a5d1fd8253bac800020f
Ruben <rubenf85@gmail.com>
2026-07-20 22:53:39 +0200

    Menu Page added along with menu service that handles the persistence of the menu items as well as a new recipe page

--- src/app/pages/menu-page/menu-page.ts
+++ src/app/pages/menu-page/menu-page.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MenuService, Recipe } from '../../services/menu.service';
 
 @Component({
   selector: 'app-menu-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu-page.html',
   styleUrl: './menu-page.scss',
 })
-export class MenuPage {}
+export class MenuPage {
+  readonly menu = inject(MenuService);
+  readonly recipeToRemove = signal<Recipe | null>(null);
+  requestRemoval(recipe: Recipe): void { this.recipeToRemove.set(recipe); }
+  cancelRemoval(): void { this.recipeToRemove.set(null); }
+  confirmRemoval(): void { const recipe = this.recipeToRemove(); if (recipe) this.menu.remove(recipe.id); this.recipeToRemove.set(null); }
+}
