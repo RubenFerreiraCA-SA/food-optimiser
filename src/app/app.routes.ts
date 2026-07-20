b1bd59967e1877b0d1f28444df1eeabd249f4e8b
Ruben <rubenf85@gmail.com>
2026-07-20 22:35:50 +0200

    Promoted Navbar to App level and h ooked up routing in home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,14 @@
 import { Routes } from '@angular/router';
-import { HomePage } from './home-page/home-page';
+import { HomePage } from './pages/home-page/home-page';
+import { PantryPage } from './pages/pantry-page/pantry-page';
+import { MenuPage } from './pages/menu-page/menu-page';
+import { PlannerPage } from './pages/planner-page/planner-page';
 
-export const routes: Routes = [{ path: '', component: HomePage }];
+export const routes: Routes = [
+  { path: '', component: HomePage },
+  { path: 'home', component: HomePage },
+  { path: 'pantry', component: PantryPage },
+  { path: 'menu', component: MenuPage },
+  { path: 'planner', component: PlannerPage },
+  { path: '**', redirectTo: 'home' },
+];
